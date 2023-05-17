e474879703eae1da22c9627c46c9d8d1ce0951bf
admin <mingaleev.d92@mail.ru>
2023-05-17 18:05:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gamecompose/ui/theme/AppShapes.kt
@@ -0,0 +1,15 @@
+package com.example.gamecompose.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * @author : Mingaleev D
+ * @data : 17.05.2023
+ */
+val AppShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(6.dp),
+    large = RoundedCornerShape(8.dp)
+)
